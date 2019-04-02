7b12eca251d691f38eaaa27a58d6784e84e9b5a1
Uathana <uathanabenhur2017@gmail.com>
2019-04-01 21:44:48 -0300

    meu premeiro comit

---
+++ script.js
@@ -0,0 +1,8 @@
+function myFunction() {
+		document.getElementById("demo").innerHTML = "Paragramph changet.";
+}
+
+function mudarTamanhoFonte() {
+	document.getElementById("demo").style.fontSize = "50px";
+
+}
